2d6e52be4b28b089dca752143248043556a7f9a5
marc-pubgears <marc@pubgears.com>
2015-11-24 21:36:15 -0500

    clean up

--- app.py
+++ app.py
@@ -6,37 +6,29 @@ from flask.ext.bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-
 def get_images():
-	images = []
-
-	for i in os.listdir(os.path.split(os.path.realpath(__file__))[0]+'/static/algae_images'):
-		if i != '.DS_Store':
-			images.append("algae_images/" + i)
-	return images
+  images = []
+  for i in os.listdir(os.path.split(os.path.realpath(__file__))[0]+'/static/algae_images'):
+    if i != '.DS_Store':
+      images.append("algae_images/" + i)
+  return images
 
 @app.route('/')
-@app.route("/index")  
+@app.route('/index')  
 def index():
 	return render_template('index.html')
 
 @app.route('/algae')
 def algae():
-	return render_template('algae.html', images=get_images())
-
-@app.route('/museum')
-def museum():
-	return render_template('museum.html')
-
+  return render_template('algae.html', images=get_images())
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+  return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
-	return render_template('500.html'), 500
+def server_error(e):
+  return render_template('500.html'), 500
 
 if __name__ == '__main__':
-	app.run()
-	
+  app.run(debug=True, threaded=True)
